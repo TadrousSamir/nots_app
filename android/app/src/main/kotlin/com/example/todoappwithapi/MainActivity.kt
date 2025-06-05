a7118f7f52eee9c13ded5ca2975b545dc555625c
unknown <tadroussamir2001@gmail.com>
2025-06-05 12:33:17 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/todoappwithapi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.todoappwithapi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:todoappwithapi/todo_list.dart';
+
+void main() {
+  runApp(const MaterialApp(home: TodoList(),
+  debugShowCheckedModeBanner: false,
+  ));
+}
+
+
